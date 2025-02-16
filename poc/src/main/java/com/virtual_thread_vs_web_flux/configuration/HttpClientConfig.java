239972b929f02cfc8ef47d4bb307e64c09597a34
Andre Parelho <parelhoneves.andre@gmail.com>
2025-02-16 19:31:33 -0300

    feat: config

---
+++ poc/src/main/java/com/virtual_thread_vs_web_flux/configuration/HttpClientConfig.java
@@ -0,0 +1,18 @@
+package com.virtual_thread_vs_web_flux.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration
+public class HttpClientConfig {
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(10))
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+    }
+}

4288f16718ea44d9bde58f494e5f7f6685ed99f2
Andre Parelho <parelhoneves.andre@gmail.com>
2025-02-16 23:11:36 -0300

    style: change name

---
+++ poc/src/main/java/com/virtual_thread_vs_web_flux/poc/virtualThread/controller/VirtualThreadFutureController.java
@@ -0,0 +1,29 @@
+package com.virtual_thread_vs_web_flux.poc.virtualThread.controller;
+
+import com.virtual_thread_vs_web_flux.poc.common.model.response.VirtualThreadResponse;
+import com.virtual_thread_vs_web_flux.poc.virtualThread.service.VirtualThreadCompletableFutureService;
+import com.virtual_thread_vs_web_flux.poc.virtualThread.service.VirtualThreadFutureService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/virtual-thread")
+public class VirtualThreadFutureController {
+    private final VirtualThreadCompletableFutureService service;
+
+    public VirtualThreadFutureController(VirtualThreadCompletableFutureService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<VirtualThreadResponse> get() throws URISyntaxException, IOException, InterruptedException {
+        VirtualThreadResponse response = this.service.get();
+        return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+    }
+}
---
+++ poc/src/main/java/com/virtual_thread_vs_web_flux/poc/virtualThread/service/VirtualThreadFutureService.java
@@ -0,0 +1,40 @@
+package com.virtual_thread_vs_web_flux.poc.virtualThread.service;
+
+import com.virtual_thread_vs_web_flux.poc.common.model.response.*;
+import com.virtual_thread_vs_web_flux.poc.virtualThread.repository.AgifyRepository;
+import com.virtual_thread_vs_web_flux.poc.virtualThread.repository.DogCeoRepository;
+import com.virtual_thread_vs_web_flux.poc.virtualThread.repository.JsonPlaceHolderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Service
+public class VirtualThreadFutureService {
+    private final JsonPlaceHolderRepository jsonPlaceHolderRepository;
+    private final DogCeoRepository dogCeoRepository;
+    private final AgifyRepository agifyRepository;
+
+    public VirtualThreadFutureService(JsonPlaceHolderRepository jsonPlaceHolderRepository, DogCeoRepository dogCeoRepository, AgifyRepository agifyRepository) {
+        this.jsonPlaceHolderRepository = jsonPlaceHolderRepository;
+        this.dogCeoRepository = dogCeoRepository;
+        this.agifyRepository = agifyRepository;
+    }
+
+    public VirtualThreadResponse get() {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            Future<ResponseCommon> agifyResponseFuture = executor.submit(this.agifyRepository::getApi);
+            Future<ResponseCommon> jsonPlaceHolderResponseFuture = executor.submit(this.jsonPlaceHolderRepository::getApi);
+            Future<ResponseCommon> dogCeoResponseFuture = executor.submit(this.dogCeoRepository::getApi);
+
+            AgifyResponse agifyResponse = (AgifyResponse) agifyResponseFuture.get();
+            JsonPlaceHolderResponse jsonPlaceHolderResponse = (JsonPlaceHolderResponse) jsonPlaceHolderResponseFuture.get();
+            DogCeoResponse dogCeoResponse = (DogCeoResponse) dogCeoResponseFuture.get();
+
+            return new VirtualThreadResponse(agifyResponse, jsonPlaceHolderResponse, dogCeoResponse);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

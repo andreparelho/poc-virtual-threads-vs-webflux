913dac6563706d2d8d7623ece883cd99b21f135d
Andre Parelho <parelhoneves.andre@gmail.com>
2025-02-17 21:10:07 -0300

    refact: refactoring responses

--- poc/src/main/java/com/virtual_thread_vs_web_flux/poc/common/model/response/AgifyResponse.java
+++ poc/src/main/java/com/virtual_thread_vs_web_flux/poc/common/model/response/AgifyResponse.java
@@ -1,31 +1,4 @@
 package com.virtual_thread_vs_web_flux.poc.common.model.response;
 
-public class AgifyResponse extends ResponseCommon {
-    private String count;
-    private String name;
-    private String age;
-
-    public String getCount() {
-        return count;
-    }
-
-    public void setCount(String count) {
-        this.count = count;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
+public record AgifyResponse(String count, String name, String age) {
 }

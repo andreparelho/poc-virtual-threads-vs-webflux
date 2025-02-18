ce214097f58b06834191ce7f416b8d7e3a43b344
Andre Parelho <parelhoneves.andre@gmail.com>
2025-02-17 21:11:10 -0300

    feat: constantts

---
+++ poc/src/main/java/com/virtual_thread_vs_web_flux/poc/virtualThread/util/VirtualThreadConstants.java
@@ -0,0 +1,24 @@
+package com.virtual_thread_vs_web_flux.poc.virtualThread.util;
+
+public class VirtualThreadConstants {
+     /*
+     *  LOGS
+     */
+    public static final String GET_METHOD = "get";
+    public static final String COMPLETABLE_FUTURE_AGIFY = "completableFutureAgify";
+    public static final String COMPLETABLE_FUTURE_JSON_PLACE_HOLDER = "completableFutureJsonPlaceHolder";
+    public static final String COMPLETABLE_FUTURE_DOG_CEO = "completableFutureDogCeo";
+    public static final String FUTURE_MESSAGE = "getting with future";
+    public static final String COMPLETABLE_FUTURE_MESSAGE = "getting with completable future";
+    public static final String ERROR_MESSAGE_FUTURE = "error future";
+    public static final String ERROR_MESSAGE_COMPLETABLE_FUTURE = "error completable future";
+
+    /*
+     *  REPOSITORY
+     */
+    public static final String JSON_PLACE_HOLDER_ENDPOINT = "/posts/1";
+    public static final String AGIFY_HOLDER_ENDPOINT = "?name=michael";
+    public static final String DOG_CEO_HOLDER_ENDPOINT = "api/breeds/image/random";
+
+
+}
